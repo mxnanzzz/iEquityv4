4b1cf77af0d12ef10449f1af3055a8ea7b656e06
Dainty Fariscal <daintyfariscal@zmzmzs-laptop.local>
2024-03-11 16:46:06 +0800

    test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ProductList } from "./Screens/ProductList.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+function App () {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Products" component={ProductList}/> 
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles= StyleSheet.create({
+    Container: {
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex:1
+
+},
+    text:{
+        fontSize: 30,
+        fontWeight:'bold'
+
+}
+
+
+})
+
+export default App;
---
+++ Screens/ProductList.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { gogetProducts} from "./Services/ProductServices.js";
+import {Products} from "./Components/Products.js"
+
+export function ProductList (){
+
+    function render ({item:Products}){
+    return(
+        <Products
+            {... Products}        
+            onPress= {() => {
+                navigation. navigate('ProductDetails', {productId: product.id})
+        
+        }}    
+        /> 
+        )
+    }
+
+    const [Products, setProducts] =useState([]);
+    useEffect(() => {
+        setProducts(gogetProducts())
+
+         
+    })
+
+        return (
+            <FlatList
+                style={styles.ProductList} 
+                contentContainerStyle={styles.productsListContainer}
+                keyExtractor={(item) => item.id.toString()} 
+                data={Products}
+                renderItem={render}
+
+            />
+
+        )
+    }
+
